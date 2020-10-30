a7f7514d28fa767d5604bd87c7ef07e49e486608
DSarceno <dsarceno68@gmail.com>
2020-10-29 20:34:34 -0600

    Caaaasi

--- Practica2/interfaz.py
+++ Practica2/interfaz.py
@@ -18,12 +18,14 @@ import matplotlib.cm as cm
 from matplotlib import animation
 from matplotlib.widgets import Button
 from matplotlib.backends.backend_gtk3cairo import FigureCanvasGTK3Cairo as FigureCanvas
+from datetime import datetime
+
 
 # Clase General
 class ventana(Gtk.Window):
     def __init__(self):
         Gtk.Window.__init__(self,title='Juego de la Vida')
-        self.set_default_size(300,300)
+        self.set_default_size(350,350)
         self.set_resizable(False)
         # self.layout = Gtk.Box()
         # self.add(self.layout)
@@ -32,7 +34,9 @@ class ventana(Gtk.Window):
 
 
         self.grid = Gtk.Grid()
-        self.add(self.grid)
+        self.grid.set_row_spacing(4)
+        self.grid.set_column_spacing(5)
+        #self.add(self.grid)
         self.box.pack_start(self.grid,True,True,0)
 
         # Create HeaderBar.
@@ -41,6 +45,9 @@ class ventana(Gtk.Window):
 
 
         #····································································
+        self.pause = False
+        self.now = datetime.now()
+        # ···································································
         # BARRA DE MENU
         mainMenuB = Gtk.MenuBar()
         # Archivo
@@ -95,9 +102,30 @@ class ventana(Gtk.Window):
 
         # self.layout.pack_start(mainMenuB,True, True, 0)
         self.hb.pack_start(mainMenuB)
-        self.grid.attach(self.hb,0,0,4,1)
+        self.grid.attach(self.hb,0,0,5,1)
         #self.box.pack_start(self.hb,True,True,0)
         # ····································································
+        self.pp = Gtk.Button('Play/Pausa')
+        self.grid.attach(self.pp,1,2,1,1)
+
+        self.savePos = Gtk.Button('Guardar')
+        self.grid.attach_next_to(self.savePos,self.pp,Gtk.PositionType.RIGHT,1,1)
+
+        self.screenShot = Gtk.Button('Screen Shot')
+        #self.grid.attach_next_to(self.screenShot,self.savePos,Gtk.PositionType.RIGHT,1,1)
+
+        self.timeIter = Gtk.SpinButton()
+        self.grid.attach_next_to(self.timeIter,self.savePos,Gtk.PositionType.RIGHT,1,1)
+
+        self.buffer = Gtk.TextBuffer()
+        self.display = Gtk.TextView(buffer = self.buffer)
+        self.display.set_size_request(30,30)
+        self.grid.attach_next_to(self.display,self.timeIter,Gtk.PositionType.RIGHT,1,1)
+        # ····································································
+
+        # Hace que los botones hagan lo que tienen que hacer
+        self.pp.connect('clicked', self.pp_clicked)
+        self.savePos.connect('clicked', self.savePos_clicked)
 
         # Hace que el menu haga lo que tiene que hacer
         archCI.connect("activate", self.archCI_activate)
@@ -107,8 +135,28 @@ class ventana(Gtk.Window):
         helpAD.connect("activate", self.helpAD_activate)
         helpCF.connect("activate", self.helpCF_activate)
 
+
         #·····································································
+    # Funcionamiento de los botones
+    # Boton de pausa
+    def pp_clicked(self, widget):
+        self.pause ^= True
+
+    # Boton de guardado de estado
+    def savePos_clicked(self, widget):
+        # FORMATO DE NOMBRES 'AñoMesDiaHoraMinutoSegundo.jvpm2'
+        name = str(self.now.year) + str(self.now.month) + str(self.now.day) + str(self.now.hour) + str(self.now.minute) + str(self.now.second) + '.jvpm2'
+        file = open(name,'w')
+        file.write(str(N) + '\n')
+        for j in range(N):
+            line = ''
+            for i in range(N):
+                line += str(gState[j,i]) + ' '
+            file.write(line + '\n')
+        file.close()
+
     # Funcionamiento del menu
+    # Cargar Configuracion inicial
     def archCI_activate(self, widget):
         dialog = Gtk.FileChooserDialog('Select a File',None,Gtk.FileChooserAction.OPEN,('Cancel',Gtk.ResponseType.CANCEL,'Ok',Gtk.ResponseType.OK))
         response = dialog.run()
@@ -122,11 +170,13 @@ class ventana(Gtk.Window):
             gState = np.zeros((N,N))
             for j in range(len(data)):
                 for i in range(len(data)):
-                    gState[j,i] = int(data[j][i])
+                    gState[j,i] = eval(data[j][i])
+            file.close()
         elif response == Gtk.ResponseType.CANCEL:
             pass
         dialog.destroy()
 
+    # Cargar configuracion aleatoria
     def archCA_activate(self, widget):
         global N
         N = random.randint(3,250)
@@ -231,13 +281,6 @@ class ventana(Gtk.Window):
                         ngState[i, j] = 0
             return ngState
 
-
-        pause = False # Pausa
-        '''
-        def onClick(event):
-            global pause
-            pause ^= True
-        '''
         # Creamos la figura, formateo diverso
         fig = plt.figure(figsize=(4, 4))
         ax = fig.add_subplot(111)
@@ -252,27 +295,21 @@ class ventana(Gtk.Window):
 
         def animate(i):
             global gState
-            if not pause:
-                print(i)
+            if not self.pause:
+                self.buffer.set_text(i)
                 gState = paso(gState)
                 imagen.set_data(gState)
 
             return imagen,
-        '''
-        # Play/Pausa
-        pause_ax = fig.add_axes((0.3, 0.025, 0.23, 0.04), anchor = 'SE')
-        pause_button = Button(pause_ax, 'Play/pause', hovercolor='0.975')
-        pause_button.on_clicked(onClick)
-        '''
 
         anim = animation.FuncAnimation(fig, animate, frames=100, blit=True, interval = 200, repeat = True)
-        #grafico = Gtk.ScrolledWindow()
-        #canvas = FigureCanvas(fig)
-        #grafico.add(canvas)
-        #self.box.pack_start(grafico,True,True,0)
-        #self.grid.attach_next_to(grafico,self.hb,Gtk.PositionType.BOTTOM,4,4)
+        grafico = Gtk.ScrolledWindow()
+        canvas = FigureCanvas(fig)
+        grafico.add(canvas)
+        #self.box.pack_start(grafico,False,False,0)
+        self.grid.attach_next_to(grafico,self.hb,Gtk.PositionType.BOTTOM,4,4)
         #self.add(grafico)
-        plt.show()
+        #plt.show()
 
 
     def conFT_activate(self,widget):
@@ -304,12 +341,6 @@ class ventana(Gtk.Window):
                         ngState[i, j] = 0
             return ngState
 
-        pause = False # Pausa
-        '''
-        def onClick(event):
-            global pause
-            pause ^= True
-        '''
         # Creamos la figura, formateo diverso
         fig = plt.figure(figsize=(4, 4))
         ax = fig.add_subplot(111)
@@ -320,22 +351,18 @@ class ventana(Gtk.Window):
             which='both',
             bottom=False,
             top=False,
-            labelbottom=False)
+            labelbottom=False,
+            labelright=False)
 
         def animate(i):
             global gState
-            if not pause:
-                print(i)
+            if self.pause == False:
+                self.buffer.set_text(i)
                 gState = paso(gState)
                 imagen.set_data(gState)
 
             return imagen,
-        '''
-        # Play/Pausa
-        pause_ax = fig.add_axes((0.3, 0.025, 0.23, 0.04), anchor = 'SE')
-        pause_button = Button(pause_ax, 'Play/pause', hovercolor='0.975')
-        pause_button.on_clicked(onClick)
-        '''
+
         anim = animation.FuncAnimation(fig, animate, frames=100, blit=True, interval = 200, repeat = True)
         plt.show()
 
