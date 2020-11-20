91c6a20db5d9a70e9d573174fcd731c0d1fd6d5b
DSarceno <dsarceno68@gmail.com>
2020-11-20 16:55:22 -0600

    Final terminado

--- Final/problema2.py
+++ Final/problema2.py
@@ -7,3 +7,27 @@
 #
 #
 #
+
+'''El programa toma dos numeros como entrada, el primero es el monto a
+retirar y el segundo el saldo total en la cuenta. El banco cobra 0.5 por cada
+retiro'''
+
+monto = input('Entrada: ')
+saldo = input()
+
+def salResult(m,s):
+    '''La funcion recibe dos numeros el monto a retirar y el saldo de la cuenta
+    con el cobro del banco de 0.50
+
+    '''
+
+    m = int(m)
+    s = round(float(s),2)
+
+    if s <= m:
+        return 'No tiene suficiente dinero'
+    else:
+        return str(s - m - 0.5) + str(0)
+
+print('-------------')
+print(salResult(monto,saldo))
--- Final/problema3.py
+++ Final/problema3.py
@@ -7,3 +7,47 @@
 #
 #
 #
+
+from math import floor
+
+def coins(n):
+    '''La función recibe un numero entero y en base a las seis denominaciones
+    de monedas que se tienen, da el numero minimo de monedas para ajustar
+    dicha cantidad.
+
+    '''
+    numC = 0
+    while n != 0:
+        if n >= 100:
+            numC += floor(n/100)
+            n = n % 100
+        elif 50 <= n < 100:
+            numC += floor(n/50)
+            n = n % 50
+        elif 10 <= n < 50:
+            numC += floor(n/10)
+            n = n % 10
+        elif 5 <= n < 10:
+            numC += floor(n/5)
+            n = n % 5
+        elif 2 <= n < 5:
+            numC += floor(n/2)
+            n = n % 2
+        elif 0 < n < 2:
+            numC += n
+            n = n % 1
+    return numC
+
+# Entrada (como se especifica en el documento)
+rep = input()
+numCoins = []
+
+for i in range(int(rep)):
+    n = input()
+    numCoins.append(coins(int(n)))
+
+print('----------------')
+
+# Salida (como se especifica en el documento)
+for i in range(int(rep)):
+    print(numCoins[i])
