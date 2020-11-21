f03e559c8f5eb6ee8a970a2622f2873c4f716736
DSarceno <dsarceno68@gmail.com>
2020-11-20 22:25:29 -0600

    Modulo Gtk

---
+++ Proyecto/proyecto.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# @Author: Diego Sarceno
+# Date: 20.11.2020
+#
+#
+#
+# -----------------------
+
+# Modulos Requeridos
+import gi
+gi.require_version('Gtk','3.0')
+from gi.repository import Gtk
