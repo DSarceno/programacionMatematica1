ee22f0b8f5884b28fd2555fccea2796440e9ef20
DSarceno <dsarceno68@gmail.com>
2020-11-25 20:14:35 -0600

    generador ascii

---
+++ Proyecto/artASCII.py
@@ -0,0 +1,88 @@
+from PIL import Image, ImageDraw, ImageFont
+import math as m
+
+
+# ascii_charact = '$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft'
+# ASCII_CHARS = list(ascii_charact)
+ASCII_CHARS = ['@','#','$','S','%','?','*','+',';',':','.',' ']
+
+
+
+def rescale(image, newWidth=100):
+    '''reescala la imagen con la misma razon de lados.
+    '''
+    (originalWidth, originalHeight) = image.size
+    ratio = originalHeight/float(originalWidth)
+    newHeight = int(ratio * newWidth)
+    newImage = image.resize((newWidth, newHeight))
+    return newImage, newHeight
+
+def escalaGrises(image):
+    '''convierte la imagen a escala de escalaGrises
+    '''
+    return image.convert('L')
+
+def pixelsAscii(image, rangeWidth=m.ceil(255/len(ASCII_CHARS))):
+    '''mapea cierto conjunto de pixeles a un caracter ascii.
+
+    0-255 lo divide en un numero de rangos de m.ceil(255/len(ASCII_CHARS)) pixeles
+    '''
+
+    # escoje en la lista de caracteres dependiendo del grupo de pixeles
+    pixelsImage = list(image.getdata())
+    pixelsChars = [ASCII_CHARS[m.floor(pixel/rangeWidth)] for pixel in
+            pixelsImage]
+
+    return "".join(pixelsChars)
+
+def imagetoAscii(image, newWidth=100):
+    '''Crea el string con el ascii generado y hace la imagen en formato .png
+    '''
+    image, newHeight = rescale(image)
+    image = escalaGrises(image)
+
+
+    pixelsChars = pixelsAscii(image)
+
+    # Crea la imagen y el archivo .txt con el asciiArt
+    fnt = ImageFont.load_default()
+    outputImage = Image.new('RGB', (10*newWidth, 12*newHeight), color = (0, 0, 0))
+    drawImage = ImageDraw.Draw(outputImage)
+
+    # file = open('asiiArt.txt','r')
+
+    for i in range(newHeight):
+        for j in range(newWidth):
+            # file.write(pixelsChars[j + i*newWidth])
+            drawImage.text((10*j, 12*i), pixelsChars[j + i*newWidth], font = fnt, fill = (255, 255, 255))
+        # file.write('\n')
+    outputImage.save('asciiArt.png')
+
+    # file.close()
+
+    # realiza los saltos de linea para imprimirlo en la terminal
+    lenPixelsChars = len(pixelsChars)
+
+    imageAscii = [pixelsChars[index: index + newWidth] for index in
+            range(0, lenPixelsChars, newWidth)]
+
+    return "\n".join(imageAscii)
+
+
+
+
+def main(file, newWidth = 100):
+    image = Image.open(file)
+
+
+    imageAscii = imagetoAscii(image)
+    print(imageAscii)
+
+
+
+
+
+import sys
+
+file = sys.argv[1]
+main(file)
--- Proyecto/proyecto.py
+++ Proyecto/proyecto.py
@@ -12,20 +12,20 @@
 import gi
 gi.require_version('Gtk','3.0')
 from gi.repository import Gtk
+from gi.repository import GdkPixbuf
 from datetime import datetime
 
 class ascii(Gtk.Window):
     def __init__(self):
         Gtk.Window.__init__(self, title = 'Arte ASCII')
-        self.set_default_size(350,100)
+        self.set_default_size(1000,400)
         self.set_resizable(False)
         self.box = Gtk.VBox()
         self.add(self.box)
 
-
         self.grid = Gtk.Grid()
-        self.grid.set_row_spacing(4)
-        self.grid.set_column_spacing(5)
+        self.grid.set_row_spacing(1)
+        self.grid.set_column_spacing(1)
         #self.add(self.grid)
         self.box.pack_start(self.grid,True,True,0)
 
@@ -82,15 +82,30 @@ class ascii(Gtk.Window):
         mainMenuB.append(helpMenuName)
 
         # self.layout.pack_start(mainMenuB,True, True, 0)
+        self.box1 = Gtk.HBox()
         self.hb.pack_start(mainMenuB)
-        self.grid.attach(self.hb,0,0,5,1)
-        #self.box.pack_start(self.hb,True,True,0)
+        #self.grid.attach(self.hb,0,0,10,1)
+        self.box1.pack_start(self.hb,True,True,100)
+        self.grid.attach(self.box1,0,0,20,1)
 
         # ACCIONES DEL MENU
+        imgCI.connect('activate', self.imgCI_activate)
         helpAD.connect('activate', self.helpAD_activate)
 
 
-
+    def imgCI_activate(self, widget):
+        dialog = Gtk.FileChooserDialog('Select a File',None,Gtk.FileChooserAction.OPEN,('Cancel',Gtk.ResponseType.CANCEL,'Ok',Gtk.ResponseType.OK))
+        response = dialog.run()
+        if response == Gtk.ResponseType.OK:
+            bx = Gtk.VBox()
+            imagen = GdkPixbuf.Pixbuf.new_from_file_at_scale(dialog.get_filename(), 100, 100,False)
+            imagenC = Gtk.Image()
+            imagenC.set_from_pixbuf(imagen)
+            bx.pack_start(imagenC,True,True,0)
+            self.grid.attach_next_to(imagenC,self.box1,Gtk.PositionType.BOTTOM,10,10)
+        elif response == Gtk.ResponseType.CANCEL:
+            pass
+        dialog.destroy()
 
     def helpAD_activate(self, widget):
         #acerca de dialogo
@@ -98,7 +113,7 @@ class ascii(Gtk.Window):
         adD = Gtk.AboutDialog()
         adD.set_program_name('Arte ASCII')
         adD.set_version('Programación Matemática 1')
-        adD.set_authors('DSR')
+        #adD.set_authors('DSR')
         adD.set_copyright('Copyright © 2020 Diego Sarceño')
         adD.set_comments('Conversor de imágenes a Artes ASCII, programado con e lenguaje de Python 3.')
         adD.set_website('https://github.com/DSarceno/programacionMatematica1/blob/master/Proyecto/proyecto.py')
